d412f6d781d69eea6b2015966012f3abc4a07fcd
4jax <dadelos.ddls@gmail.com>
2024-01-06 17:20:09 +0000

    Create Conditionals.js

---
+++ 6_Conditionals.js
@@ -0,0 +1,41 @@
+// Conditions - if/else statements where it runs something base on the 
+//              if condition being true or false or else do else statement
+
+// if can be alone, paired with "else" or nest with more "else if"
+
+let age = 15;
+
+// Lets say i want to console log if age is above 18
+if(age > 18) {
+    console.log('You are above 18');
+} 
+else if (age <= 0 ){  // No one could be below 0 years old right?
+    console.log('Invalid age');
+}
+else { // I'll also console log something else for those who are below 18
+    console.log('You are underage')
+}
+
+// You can also nest if else in side any if else statement
+// we'll use the same age but this time to check for eligibility to drive
+
+// We will also add a new variable to check for license
+let hasLicense = false;
+
+if(age > 16) {
+    console.log('Youre eligible to drive');
+    // Here we can also add an condition pay attention to the tab
+    if(hasLicense){
+        console.log('You can drive');
+    } else {
+        console.log('You need a license');
+    }
+} 
+else if (age <= 0 ){  
+    console.log('Invalid age');
+}
+else {
+    console.log('You are underage')
+}
+
+
