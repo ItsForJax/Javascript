038aa8d9f3066c991efc479e1afd46c47081840c
4jax <dadelos.ddls@gmail.com>
2024-01-07 17:47:34 +0000

    Update Logic.js

--- 9_Logic.js
+++ 9_Logic.js
@@ -34,3 +34,26 @@ if (bigBurger) {
 let notBigBurger = !isBig;
 
 console.log(`is the burger big: ${notBigBurger}`);
+
+// Since we are talking boolean values, lets talk about comaparison operators
+
+// Equal to (==)
+// Not equal to (!=)
+// Strict equal to (===)
+// Strict not equal to (!==)
+
+// Equal to is essentially AND as long as 2 inputs are the same its true
+// regardless of data type
+// example:
+
+let Pi = 3.14; // number data type
+
+// we compared number Pi to string "3.14" using == operator which is true
+Pi == "3.14" ? console.log("its Equal") : console.log("not equal")
+
+// same goes with the not equal
+Pi != "3.14" ? console.log("not equal") : console.log("its Equal")
+
+// now with strict equal and not equal
+Pi === "3.14" ? console.log("strictly equal") : console.log("not strictly equal")
+Pi !== "3.14" ? console.log("strictly not equal") : console.log("strictly equal")
